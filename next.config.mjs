a1ccc2b0465746fdacfb4ee3c324d0c39ff90134
krishnarao <krishna.r@simpragma.com>
2024-10-12 19:12:29 +0530

    added github yml file

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,18 @@
-
-
-  /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  env: {
+    customKey: 'my-value',
+  },
+  // Runtime configuration
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: process.env.MY_SECRET,
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    staticFolder: '/static',
+  },
 }
 
 export default nextConfig
